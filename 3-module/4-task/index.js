35612e9893d98806c651ee259ba5403708e2f468
NameKonstantinBerg <email@example.rukonstantin.dudukin.00@gmail.com>
2024-08-01 08:10:03 +0700

    solutions

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -23,17 +23,3 @@ function showSalary(users, age) {
     .map(item => `${item.name}, ${item.balance}`)
     .join('\n');
 }
-
-/* let result = showSalary(users, age);
-
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let users = [user1, <...и другие пользователи>] */
