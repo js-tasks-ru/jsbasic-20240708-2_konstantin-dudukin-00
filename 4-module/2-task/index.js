5cd75ce99692b2c1bd5093a70a544b7b55967ba6
NameKonstantinBerg <email@example.rukonstantin.dudukin.00@gmail.com>
2024-08-04 21:47:02 +0700

    Module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+
+  for (let counter = 0; counter < table.rows.length; counter++) {
+    tableCells = table.rows[counter].cells[counter];
+    tableCells.style.backgroundColor = 'red';
+  }
+  
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  let tableRows = table.rows;
+
+  // Проходим по Status
+  for (let i = 1; i < table.rows.length; i++) {
+    if(tableRows[i].children[3].dataset.available === 'true') {
+      tableRows[i].children[3].classList.add('available');
+    } else if(tableRows[i].children[3].dataset.available === 'false') {
+      tableRows[i].children[3].classList.add('unavailable');
+    } else if(tableRows[i].children[3].dataset.available === undefined) {
+        tableRows[i].children[3].hidden = true;
+    }
+  }
+
+  // Проходим по Gender
+  for (let i = 1; i < table.rows.length; i++) {
+    if(tableRows[i].children[2].innerText === 'm') {
+      tableRows[i].children[2].classList.add('male');
+    } else {
+      tableRows[i].children[2].classList.add('female');
+    }
+  }
+
+  // Проходим по Age
+  for (let i = 1; i < table.rows.length; i++) {
+    let age = tableRows[i].children[1].innerText;
+
+    if(parseInt(age) <= 18) {
+      tableRows[i].children[1].style="text-decoration: line-through";
+    }
+  }
 }
